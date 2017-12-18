4994a63953bb75bbf63815a81486e74fcfb0524a
RebeccaVonRuden <rebecca@vonruden.info>
2017-12-17 18:10:22 -0600

    Java FizzBuzz Final

--- rvonruden-app-app1/src/main/java/com/rvonruden/app/App.java
+++ rvonruden-app-app1/src/main/java/com/rvonruden/app/App.java
@@ -1,19 +1,58 @@
 package com.rvonruden.app;
 
 /**
- * Hello world!
  *
+ * Fizz Buzz is a game were any multple of three is Fizz.
+ * Any mutilple of five is Buzz and any mulptple of five and three is Fizz Buzz.
+ *
+ * All Code is unit tested in AppTest.java to prove the above buisness rules are enforced
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( methodToTest(1) );
+        System.out.println( "Welcome to FizzBuzz!" );
+        for (int i = 1; i <= 100; i++)
+            {
+            System.out.println( FizzBuzz(i) );
+
+            }
     }
 
-    public static String methodToTest(int input)
+    public static String FizzBuzz(int input)
     {
-        return "1";
+      if (input % 3 == 0 && input % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else if (input % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else if (input % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else 
+        {
+            return Integer.toString(input);
+        } 
+// incorrect code to show kids at the elementry school feild trip.
+       /* if (input % 3 == 0)
+        {
+            return "Fizz";
+        }
+        else if (input % 5 == 0)
+        {
+            return "Buzz";
+        }
+        else if (input % 3 == 0 && input % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else 
+        {
+            return Integer.toString(input);
+        } */
     }
 }
--- rvonruden-app-app1/src/test/java/com/rvonruden/app/AppTest.java
+++ rvonruden-app-app1/src/test/java/com/rvonruden/app/AppTest.java
@@ -5,7 +5,8 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for Fizz Buzz App.
+ * All Rules for Fizz Buzz apply.
  */
 public class AppTest 
     extends TestCase
@@ -31,9 +32,117 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testFizzBuzz_Given_1_Expect_1()
     {
-        String actual = App.methodToTest(1);
+        String actual = App.FizzBuzz(1);
         assertEquals("1", actual);
+    }
+    
+    public void testFizzBuzz_Given_2_Expect_2()
+    {
+        String actual = App.FizzBuzz(2);
+        assertEquals("2", actual);
+    }
+
+    public void testFizzBuzz_Given_3_Expect_Fizz()
+    {
+        String actual = App.FizzBuzz(3);
+        assertEquals("Fizz", actual);
+    }
+
+    public void testFizzBuzz_Given_4_Expect_4()
+    {
+        String actual = App.FizzBuzz(4);
+        assertEquals("4", actual);
+    }
+
+    public void testFizzBuzz_Given_5_Expect_Buzz()
+    {
+        String actual = App.FizzBuzz(5);
+        assertEquals("Buzz", actual);
+    }
+
+    public void testFizzBuzz_Given_6_Expect_Fizz()
+    {
+        String actual = App.FizzBuzz(6);
+        assertEquals("Fizz", actual);
+    }
+    
+    public void testFizzBuzz_Given_7_8_Expect_7_8()
+    {
+        String actual = App.FizzBuzz(7);
+        assertEquals("7", actual);
+
+        actual = App.FizzBuzz(8);
+        assertEquals("8", actual);
+    }
+
+    public void testFizzBuzz_Given_9_Expect_Fizz()
+    {
+        String actual = App.FizzBuzz(9);
+        assertEquals("Fizz", actual);
+    }
+
+    public void testFizzBuzz_Given_10_Expect_Buzz()
+    {
+        String actual = App.FizzBuzz(10);
+        assertEquals("Buzz", actual);
+    }
+
+    public void testFizzBuzz_Given_12_Expect_Fizz()
+    {
+        String actual = App.FizzBuzz(12);
+        assertEquals("Fizz", actual);
+    }
+
+    public void testFizzBuzz_Given_15_Expect_FizzBuzz()
+    {
+        String actual = App.FizzBuzz(15);
+        assertEquals("FizzBuzz", actual);
+    }
+
+    public void testFizzBuzz_Given_multple_of_3_Expect_Fizz()
+    {
+        String actual = App.FizzBuzz(3);
+        assertEquals("Fizz", actual);
+
+        actual = App.FizzBuzz(6);
+        assertEquals("Fizz", actual);
+
+        actual = App.FizzBuzz(9);
+        assertEquals("Fizz", actual);
+
+        actual = App.FizzBuzz(12);
+        assertEquals("Fizz", actual);
+    }
+
+    public void testFizzBuzz_Given_multple_of_5_Expect_Buzz()
+    {
+        String actual = App.FizzBuzz(5);
+        assertEquals("Buzz", actual);
+
+        actual = App.FizzBuzz(10);
+        assertEquals("Buzz", actual);
+
+        actual = App.FizzBuzz(20);
+        assertEquals("Buzz", actual);
+
+        actual = App.FizzBuzz(25);
+        assertEquals("Buzz", actual);
+    }
+
+    public void testFizzBuzz_Given_multple_of_3_and_5_Expect_FizzBuzz()
+    {
+        String actual = App.FizzBuzz(15);
+        assertEquals("FizzBuzz", actual);
+
+        actual = App.FizzBuzz(30);
+        assertEquals("FizzBuzz", actual);
+
+        actual = App.FizzBuzz(45);
+        assertEquals("FizzBuzz", actual);
+
+        actual = App.FizzBuzz(60);
+        assertEquals("FizzBuzz", actual);
     }
 }
