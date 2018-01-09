77dfdac40dc932e95b6d49dcf4ac471e958bc648
RebeccaVonRuden <rebecca@vonruden.info>
2018-01-08 19:56:19 -0600

    Roman Numeral Program

--- RomanNumeral-app/src/main/java/com/rvonruden/app/App.java
+++ RomanNumeral-app/src/main/java/com/rvonruden/app/App.java
@@ -1,19 +1,33 @@
 package com.rvonruden.app;
-
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "The Roman numeral is " + Romanize(1) );
+        System.out.println( "The Roman numeral is " + Romanize() );
         
     }
 
-    public static String Romanize(Integer input) 
+    public static String Romanize(int input) 
     {
-        return "I";
+        int i = input;
+        String result = "";
+        // Special Roman numerals mapping
+        int[] numbers = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] numerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        // Prosses each numeral before checking the next one in order of highest to lowest.
+        for(int n = 0; n < numbers.length; n++)
+        {
+            // Check for multiples of of this numeral
+            while(i >= numbers[n])
+            {
+                result = result + numerals[n];
+                i = i - numbers[n];
+            }
+
+        }
+      
+        return result;
+
     }
 }
--- RomanNumeral-app/src/test/java/com/rvonruden/app/AppTest.java
+++ RomanNumeral-app/src/test/java/com/rvonruden/app/AppTest.java
@@ -36,4 +36,137 @@ public class AppTest
         String actual = App.Romanize(1);
         assertEquals("I", actual);
     }
+
+    public void test_Given_2_Expect_Roman_Numeral_II()
+    {
+        String actual = App.Romanize(2);
+        assertEquals("II", actual);
+    }
+
+    public void test_Given_3_Expect_Roman_Numeral_III()
+    {
+        String actual = App.Romanize(3);
+        assertEquals("III", actual);
+    }
+
+    public void test_Given_4_Expect_Roman_Numeral_IV()
+    {
+        String actual = App.Romanize(4);
+        assertEquals("IV", actual);
+    }
+
+    public void test_Given_5_Expect_Roman_Numeral_V()
+    {
+        String actual = App.Romanize(5);
+        assertEquals("V", actual);
+    }
+
+    public void test_Given_6_Expect_Roman_Numeral_VI()
+    {
+        String actual = App.Romanize(6);
+        assertEquals("VI", actual);
+    }
+
+    public void test_Given_7_Expect_Roman_Numeral_VII()
+    {
+        String actual = App.Romanize(7);
+        assertEquals("VII", actual);
+    }
+
+    public void test_Given_8_Expect_Roman_Numeral_VIII()
+    {
+        String actual = App.Romanize(8);
+        assertEquals("VIII", actual);
+    }
+
+    public void test_Given_9_Expect_Roman_Numeral_IX()
+    {
+        String actual = App.Romanize(9);
+        assertEquals("IX", actual);
+    }
+
+    public void test_Given_10_Expect_Roman_Numeral_X()
+    {
+        String actual = App.Romanize(10);
+        assertEquals("X", actual);
+    }
+
+    public void test_Given_11_Expect_Roman_Numeral_XI()
+    {
+        String actual = App.Romanize(11);
+        assertEquals("XI", actual);
+    }
+
+    public void test_Given_12_Expect_Roman_Numeral_XII()
+    {
+        String actual = App.Romanize(12);
+        assertEquals("XII", actual);
+    }
+
+    public void test_Given_13_Expect_Roman_Numeral_XIII()
+    {
+        String actual = App.Romanize(13);
+        assertEquals("XIII", actual);
+    }
+
+    public void test_Given_14_Expect_Roman_Numeral_XIV()
+    {
+        String actual = App.Romanize(14);
+        assertEquals("XIV", actual);
+    }
+
+    public void test_Given_15_Expect_Roman_Numeral_XV()
+    {
+        String actual = App.Romanize(15);
+        assertEquals("XV", actual);
+    }
+
+    public void test_Given_39_Expect_Roman_Numeral_XXXIX()
+    {
+        String actual = App.Romanize(39);
+        assertEquals("XXXIX", actual);
+    }
+
+    public void test_Given_563_Expect_Roman_Numeral_DLXIII()
+    {
+        String actual = App.Romanize(563);
+        assertEquals("DLXIII", actual);
+    }
+         public void test_Given_39_Expect_Roman_Numeral_XXXIX()
+    {
+        String actual = App.Romanize(39);
+        assertEquals("XXXIX", actual);
+    }
+    public void test_Given_1871_Expect_Roman_Numeral_MDCCCLXXI()
+    {
+        String actual = App.Romanize(1871);
+        assertEquals("MDCCCLXXI", actual);
+    }
+
+    public void test_Given_1900_Expect_Roman_Numeral_MCM()
+    {
+        String actual = App.Romanize(1900);
+        assertEquals("MCM", actual);
+    }
+
+    public void test_Given_2018_Expect_Roman_Numeral_MMXVIII()
+    {
+        String actual = App.Romanize(2018);
+        assertEquals("MMXVIII", actual);
+    }
+
+    public void test_Given_397_Expect_Roman_Numeral_CCCXCVII()
+    {
+        String actual = App.Romanize(397);
+        assertEquals("CCCXCVII", actual);
+    }
+
+    public void test_Given_54_Expect_Roman_Numeral_LIV()
+    {
+        String actual = App.Romanize(54);
+        assertEquals("LIV", actual);
+    }
+      
+
+
 }
